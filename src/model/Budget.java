4f244443b0faf05a1fdb39bdfe96409399d98509
Peyton Hislop <peyton.hislop12@gmail.com>
2025-02-06 22:27:35 -0500

    Put the files in the correct folders

---
+++ src/model/Budget.java
@@ -0,0 +1,27 @@
+package model;
+
+public class Budget {
+    private double monthlyLimit;
+    private double weeklyLimit;
+
+    public Budget(double monthlyLimit, double weeklyLimit) {
+        this.monthlyLimit = monthlyLimit;
+        this.weeklyLimit = weeklyLimit;
+    }
+
+    public double getMonthlyLimit() {
+        return monthlyLimit;
+    }
+
+    public double getWeeklyLimit() {
+        return weeklyLimit;
+    }
+
+    public void setMonthlyLimit(double monthlyLimit) {
+        this.monthlyLimit = monthlyLimit;
+    }
+
+    public void setWeeklyLimit(double weeklyLimit) {
+        this.weeklyLimit = weeklyLimit;
+    }
+}
