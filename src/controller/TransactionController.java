d0ae24e8e74c810d6e0aaeac89ffd20122a48c3c
Peyton Hislop <peyton.hislop12@gmail.com>
2025-02-09 11:34:21 -0500

    base of transaction classes

--- src/controller/TransactionController.java
+++ src/controller/TransactionController.java
@@ -1,5 +1,25 @@
 package controller;
-
+import java.util.ArrayList;
+import java.util.List;
+import model.Transaction;
 public class TransactionController {
+	private List<Transaction> transactions;
 
+    public TransactionController() {
+        transactions = new ArrayList<>();
+    }
+
+    public void addTransaction(String date, String description, double amount) {
+        transactions.add(new Transaction(date, description, amount));
+    }
+
+    public void displayTransactions() {
+        if (transactions.isEmpty()) {
+            System.out.println("No transactions available.");
+        } else {
+            for (Transaction transaction : transactions) {
+                System.out.println(transaction);
+            }
+        }
+    }
 }
--- src/model/Transaction.java
+++ src/model/Transaction.java
@@ -1,5 +1,33 @@
 package model;
 
-public class Transaction {
 
+
+// Transaction class to store transaction details
+public class Transaction {
+    private String date;
+    private String description;
+    private double amount;
+
+    public Transaction(String date, String description, double amount) {
+        this.date = date;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Date: " + date + ", Description: " + description + ", Amount: $" + amount;
+    }
 }
