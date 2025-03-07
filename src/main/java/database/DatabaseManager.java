7d8e1f5e41b7f38723832a5ebff92968c93f15c1
K.K <100506531+KhalifasKhalifa@users.noreply.github.com>
2025-03-07 17:56:00 -0500

    added             <dependency>                 <groupId>org.postgresql</groupId>                 <artifactId>postgresql</artifactId>                 <version>42.7.5</version>             </dependency>

--- src/main/java/database/DatabaseManager.java
+++ src/main/java/database/DatabaseManager.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseManager {
-    private static final String URL = "jdbc:postgresql://localhost:5432/demodb"; // change if needed
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "joshuang0827"; // Replace with your actual PostgreSQL password
+    private static final String URL = "jdbc:postgresql://localhost:5432/Projecttest"; // change if needed
+    private static final String USER = "khalifa";
+    private static final String PASSWORD = "";
 
     // Always return a new connection
     public static Connection getConnection() throws SQLException {
